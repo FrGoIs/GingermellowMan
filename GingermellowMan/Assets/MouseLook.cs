8972f83bb07a2d77456a9bf397312010fbf4e1a3
Francisco <franciscogisas@gmail.com>
2020-10-06 13:20:08 -0500

    Import Models

---
+++ GingermellowMan/Assets/MouseLook.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField]
+    private float mouseSensitivity = 100f;
+    float mouseX, mouseY;
+   
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+    }
+}
