10cf4e5edbe06eb211dc45dd13cd62a04c780c65
Francisco <franciscogisas@gmail.com>
2020-10-06 21:33:07 -0500

    Better Player Movement

---
+++ GingermellowMan/Assets/Scripts/PlayerScripts/MouseLook.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLook : MonoBehaviour
+{
+    [SerializeField]
+    private float mouseSensitivity = 100f;
+    float mouseX, mouseY, xRotation;
+    [SerializeField]
+    Transform playerBody;
+    // Update is called once per frame
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        Debug.Log(xRotation);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ GingermellowMan/Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Animations;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField]
+    CharacterController controller;
+    [SerializeField]
+    float speed = 12;
+    float x, z;
+    private void Start()
+    {
+        
+    }
+    //Pensar sobre ventajas de declarar global vs local en este caso.
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * speed * Time.deltaTime);
+    }
+
+
+
+
+}
+
